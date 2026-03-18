60dc664b70079d5489cc08e9f6e0edc95eb149e9
FaellEmiliano <75170691+FaellEmiliano@users.noreply.github.com>
2026-03-18 14:19:36 -0300

    Update projeto.js

--- WEB1/projeto.js
+++ WEB1/projeto.js
@@ -7,13 +7,13 @@ let qntd_cap = 10;
 let cap_p_dia = qntd_cap/dias_prova;
 
 if (cap_p_dia >=5){
-    console.log("Cronograma pesado! Tem que estudar ",cap_p_dia.toFixed(2)," capitulos por dia")
+    console.log("Cronograma pesado! Tem que estudar ",cap_p_dia," capitulos por dia")
 }
 if (cap_p_dia <=2){
-    console.log("Cronograma leve! Tem que estudar ",cap_p_dia.toFixed(2)," capitulos por dia")
+    console.log("Cronograma leve! Tem que estudar ",cap_p_dia," capitulos por dia")
 }
 if (2<cap_p_dia && cap_p_dia<5 ){
-    console.log("Cronograma médio! Tem que estudar ",cap_p_dia.toFixed(2)," capitulos por dia")
+    console.log("Cronograma médio! Tem que estudar ",cap_p_dia," capitulos por dia")
 }
 
 
